0b00f0f88de0b562c889f78ac0cf6f1f97eab949
aledelorbe <mgranadosb1800@alumno.ipn.mx>
2024-12-09 12:57:39 -0600

    first commit

---
+++ contadorcalorias/src/main/java/com/alejandro/contadorcalorias/ContadorcaloriasApplication.java
@@ -0,0 +1,13 @@
+package com.alejandro.contadorcalorias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContadorcaloriasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContadorcaloriasApplication.class, args);
+	}
+
+}
---
+++ contadorcalorias/src/test/java/com/alejandro/contadorcalorias/ContadorcaloriasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alejandro.contadorcalorias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContadorcaloriasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
