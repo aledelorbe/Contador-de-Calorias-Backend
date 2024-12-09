8cdbf91fd38baf49d9caa47125812193847a3d21
aledelorbe <mgranadosb1800@alumno.ipn.mx>
2024-12-09 13:04:28 -0600

    create entity class

---
+++ contadorcalorias/src/main/java/com/alejandro/contadorcalorias/entities/Activity.java
@@ -0,0 +1,47 @@
+package com.alejandro.contadorcalorias.entities;
+
+public class Activity {
+
+    private String id;
+
+    private String category;
+
+    private String name;
+
+    private int calories;
+
+    public Activity() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+}
