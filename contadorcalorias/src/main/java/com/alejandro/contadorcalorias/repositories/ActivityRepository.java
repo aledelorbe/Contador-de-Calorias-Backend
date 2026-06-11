86754ae4cef0af40487a735671b2306299608447
aledelorbe <alejandro.magb@gmail.com>
2026-06-11 14:53:22 -0600

    details

--- contadorcalorias/src/main/java/com/alejandro/contadorcalorias/repositories/ActivityRepository.java
+++ contadorcalorias/src/main/java/com/alejandro/contadorcalorias/repositories/ActivityRepository.java
@@ -1,12 +1,10 @@
 package com.alejandro.contadorcalorias.repositories;
 
 import com.alejandro.contadorcalorias.entities.Activity;
-import org.springframework.stereotype.Repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 // The interface called 'MongoRepository' allows the use of crud operations
-@Repository
 public interface ActivityRepository extends MongoRepository<Activity, String> {
 
 } 
